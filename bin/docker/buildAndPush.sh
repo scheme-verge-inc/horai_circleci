95953f1f691ca897ebb00953bb8075cb5e6f76ed
Shinya Sugamoto <sugamoto@me.com>
2020-08-24 02:16:36 +0900

    dockerhubへのpushスクリプト追加

---
+++ bin/docker/buildAndPush.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
+PROJECT_ROOT=$SCRIPT_DIR/../..
+IMAGE_DIR=$PROJECT_ROOT/.circleci/images
+
+cd $IMAGE_DIR
+
+docker build --no-cache -t horai/api-circleci-image . -f Dockerfile.nodejs
+docker build --no-cache -t horai/python-circleci-image . -f Dockerfile.python
+
+docker push horai/api-circleci-image:latest
+docker push horai/python-circleci-image:latest
