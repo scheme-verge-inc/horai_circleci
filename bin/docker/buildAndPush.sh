7d3c9318403d0bf18750e1bf5e482db90e7e8e1f
Shinya Sugamoto <sugamoto@me.com>
2021-11-27 23:34:49 +0900

    feat: create elasticsearch Dockerfile

--- bin/docker/buildAndPush.sh
+++ bin/docker/buildAndPush.sh
@@ -9,7 +9,9 @@ IMAGE_DIR=$PROJECT_ROOT/.circleci/images
 cd $IMAGE_DIR
 
 docker build --no-cache -t horai/api-circleci-image . -f Dockerfile.nodejs
+docker build --no-cache -t horai/elasticsearch . -f Dockerfile.elasticsearch
 docker build --no-cache -t horai/python-circleci-image . -f Dockerfile.python
 
 docker push horai/api-circleci-image:latest
+docker push horai/elasticsearch:latest
 docker push horai/python-circleci-image:latest
